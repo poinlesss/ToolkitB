18db104fa04a403e130bf440d91d898ca097385b
poinless <revans@pen-tec.com>
2021-08-15 09:44:42 -0500

    added read_clipboard_simple.py

---
+++ read_clipboard_simple.py
@@ -0,0 +1,11 @@
+def read_from_clipboard():
+    import subprocess
+
+    current_clipboard_text = subprocess.check_output(['pbpaste'])
+    return current_clipboard_text.decode('utf-8')
+
+
+if __name__ == '__main__':
+    clipboard_text = read_from_clipboard()
+    print(clipboard_text)
+
--- write_to_clipboard_simple.py
+++ write_to_clipboard_simple.py
@@ -1,8 +1,8 @@
-def write_to_clipboard(output):
+def write_to_clipboard(new_clipboard_text):
     import subprocess
     process = subprocess.Popen('pbcopy', env={'LANG': 'en_US.UTF-8'}, stdin=subprocess.PIPE)
-    process.communicate(output.encode())
+    process.communicate(new_clipboard_text.encode())
 
 
 if __name__ == '__main__':
-    write_to_clipboard("you have written to the clipboard.")
+    write_to_clipboard("you have written to the clipboard")
