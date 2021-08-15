ee16f032ec8adc8dc3318ed61f24c6963c3ce795
poinless <revans@pen-tec.com>
2021-08-15 03:49:51 -0500

    added underscore to script name and updated to production level

---
+++ delete_directory_contents.py
@@ -0,0 +1,33 @@
+#expects one parameter (the target folder)
+
+import sys
+import os, shutil
+from os.path import expanduser
+
+home_directory = expanduser("~")
+
+n = len(sys.argv)
+
+arg1 = ''
+
+for i in range(1, n):
+    if i == 1:
+        arg1 = sys.argv[i]
+
+if arg1:
+    folder = arg1
+else:
+    folder = home_directory + '/09fdmsif'
+
+try:
+    for filename in os.listdir(folder):
+        file_path = os.path.join(folder, filename)
+        try:
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                os.unlink(file_path)
+            elif os.path.isdir(file_path):
+                shutil.rmtree(file_path)
+        except Exception as e:
+            print('Failed to delete %s. Reason: %s' % (file_path, e))
+except Exception as e:
+    print('No argument passed. \n%s' % e)
